5be159a80e7c246a22f77f58da72a895b5c66266
k4b00m-Hozaifa <47463707+k4b00m-Hozaifa@users.noreply.github.com>
2019-02-09 18:49:20 -0500

    Update Find_Gene.java

--- Find_Gene.java
+++ Find_Gene.java
@@ -5,29 +5,40 @@ import edu.duke.*;
  * @author Hozaifa Owaisi 
  * @version 1.0
  */
-public class Find_Gene {
+public class Find_Gene{
     public String FindGene(String dna) {
         int start_gene = dna.indexOf("ATG");
-        int curr_gene = dna.indexOf("TAA",start_gene+3);
-        int multiple = (start_gene-curr_gene)%3;
         if (start_gene == -1) {
             return "NO GENES";
         }
-        
-        while (curr_gene != -1) {
-            if ((curr_gene-start_gene)%3 == 0) {
-             return dna.substring(start_gene,curr_gene+3);
-            }
-            else {
-              curr_gene = dna.indexOf("TAA",curr_gene+1);
-            }
+        int taa_Index = firstStopCodon(dna,start_gene,"TAA");
+        int tag_Index = firstStopCodon(dna,start_gene,"TAG");
+        int tga_Index = firstStopCodon(dna,start_gene,"TGA");
+        int temp_min = Math.min(taa_Index,tag_Index);
+        int min_index = Math.min(temp_min,tga_Index);
+        if(min_index == dna.length()) {
+            return "NO GENE";
         }
-        return "NO GENE";
+        return dna.substring(start_gene,min_index+3);
     }
     
-    public void testFind() {
+    public int firstStopCodon(String dna_sur, int startIndex, String StopCodon) {
+        int curr_Index = dna_sur.indexOf(StopCodon,startIndex+3);
+        while(curr_Index != -1) {
+            if ((curr_Index - startIndex)%3 == 0) {
+                return curr_Index;
+            }
+            else {
+                curr_Index = dna_sur.indexOf(StopCodon,curr_Index+1);
+            }
+        }
+        return dna_sur.length();
+    }
     
-        FileResource dna_file = new FileResource("TEST.txt");
+    public static void main(String args) {
+    
+        FileResource dna_file = new FileResource(args);
+        System.out.println(args);
         String dna = dna_file.asString();
         Find_Gene gene = new Find_Gene();
         String result_gene = gene.FindGene(dna);
